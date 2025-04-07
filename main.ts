ff3d9dcc07dd3e370a431808b056b17a76b73eb8
kennethbchen <kennethbchen@gmail.com>
2025-04-07 13:23:40 -0400

    Name files based on journal day instead

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, FileSystemAdapter, TFile } from 'obsidian';
-import {normalizePath } from 'obsidian';
+import { normalizePath } from 'obsidian';
 import { moment } from 'obsidian';
 import * as internal from 'stream';
 
@@ -25,7 +25,7 @@ function getJournalDate(numberRolloverOffset: number): Date {
 	if (date.getHours() <= numberRolloverOffset) {
 		date.setDate(date.getDate() - 1);
 	}
-	
+
 	return date;
 }
 
@@ -36,20 +36,20 @@ function getDays(numberRolloverOffset: number): number {
 
 	var endDate: Date = getJournalDate(numberRolloverOffset);
 
-	return Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));	
+	return Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
 }
 
 async function openOrCreateEntry(destinationFolder: string, title: string, template: string = ""): Promise<void> {
 	var filePath: string = normalizePath(destinationFolder + title) + ".md";
-	
+
 	// Create folder if not exists
 	if (! await this.app.vault.adapter.exists(destinationFolder)) {
 		this.app.vault.createFolder(destinationFolder);
 	}
-	
+
 	// Create file if not exists
 	if (! await this.app.vault.adapter.exists(filePath)) {
-		
+
 		if (template !== "") {
 			await this.app.vault.create(filePath, template);
 		} else {
@@ -57,15 +57,15 @@ async function openOrCreateEntry(destinationFolder: string, title: string, templ
 		}
 
 		new Notice("Created " + filePath);
-		
+
 	}
 
 	// Open in active leaf
-	
+
 	const file = await this.app.vault.getFileByPath(filePath);
-	
+
 	var leaf = this.app.workspace.getLeaf(false);
-	
+
 	await leaf.openFile(file);
 
 	new Notice("Opened " + filePath);
@@ -75,13 +75,13 @@ async function openOrCreateEntry(destinationFolder: string, title: string, templ
 async function readTemplateData(templatePath: string): Promise<string> {
 
 	var validTemplateExists = templatePath !== "" && await this.app.vault.adapter.exists(templatePath);
-	
+
 	var templateData = "";
 
 	// Check if there is a template to apply
 	if (validTemplateExists) {
 		// Read template
-		
+
 		templateData = await this.app.vault.adapter.read(templatePath);
 
 	}
@@ -104,19 +104,19 @@ export default class DailyJournalHelper extends Plugin {
 				var date = getJournalDate(this.settings.numberRolloverOffset);
 				var number = getDays(this.settings.numberRolloverOffset).toString();
 
+
 				// Fill in template data if needed
 				templateData = templateData.replace("{{ date }}", moment().format());
 				templateData = templateData.replace("{{ journal_date }}", moment(date).hour(0).minute(0).second(0).format());
 				templateData = templateData.replace("{{ journal_number }}", number);
-
-				var fileName = `${this.settings.filenamePrefix}${number}`;
+				var fileName = `${this.settings.filenamePrefix}${moment(date).hour(0).minute(0).second(0).format("YYYY-MM-DD")}`;
 
 				openOrCreateEntry(this.settings.fileDestination.toString(), fileName, templateData);
 			});
 
 
-			
-			
+
+
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -146,7 +146,7 @@ class DailyJournalSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
@@ -171,32 +171,32 @@ class DailyJournalSettingTab extends PluginSettingTab {
 					this.plugin.settings.fileDestination = value;
 					await this.plugin.saveSettings();
 				})
-				);
+			);
 
 		new Setting(containerEl)
-		.setName('Journal Template Location')
-		.setDesc('Location to file containing daily journal template')
-		.addText(text => text
-			.setPlaceholder("E.g 'Template.md'")
-			.setValue(this.plugin.settings.templatePath.toString())
-			.onChange(async (value) => {
-				this.plugin.settings.templatePath = value;
-				await this.plugin.saveSettings();
-			})
+			.setName('Journal Template Location')
+			.setDesc('Location to file containing daily journal template')
+			.addText(text => text
+				.setPlaceholder("E.g 'Template.md'")
+				.setValue(this.plugin.settings.templatePath.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.templatePath = value;
+					await this.plugin.saveSettings();
+				})
 			);
-		
+
 		new Setting(containerEl)
-		.setName('Journal filename prefix')
-		.setDesc('Prefix of filename for journal. Make sure to include trailing spaces if needed. "Daily Journal " becomes "Daily Journal xxxx".')
-		.addText(text => text
-			.setPlaceholder("E.g 'Daily Journal '")
-			.setValue(this.plugin.settings.filenamePrefix.toString())
-			.onChange(async (value) => {
-				this.plugin.settings.filenamePrefix = value;
-				await this.plugin.saveSettings();
-			})
+			.setName('Journal filename prefix')
+			.setDesc('Prefix of filename for journal. Make sure to include trailing spaces if needed. "Daily Journal " becomes "Daily Journal xxxx".')
+			.addText(text => text
+				.setPlaceholder("E.g 'Daily Journal '")
+				.setValue(this.plugin.settings.filenamePrefix.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.filenamePrefix = value;
+					await this.plugin.saveSettings();
+				})
 			);
-			
-			
+
+
 	}
 }
